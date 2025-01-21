38ab52bf27f59e5c04688b48719e4449119ea76f
ThanhNguyen03 <thanhnguyen25011207@gmail.com>
2025-01-21 19:20:12 +0700

    update description

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Porfolio",
-  description: "Create by ThanhNguyen03",
+  description: "Create by ThanhNguyen03 aka thanhfng_",
 };
 
 export default function RootLayout({
