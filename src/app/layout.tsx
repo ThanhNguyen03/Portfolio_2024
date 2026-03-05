966669b9757706428cf3e4a10414c7b44411dd92
ThanhNguyen03 <thanhfng.dev@gmail.com>
2026-03-05 16:15:17 +0700

    update namespace

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Porfolio',
+  title: 'Portfolio',
   description: 'Create by thanhf.ng_',
 }
 
